142d3292eb313f7265bb07b949cb1275df37a098
anooplab <anoop@chem.iitkgp.ernet.in>
2023-02-13 16:32:47 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='distances',
+    version='0.1',
+    packages=['src'],
+    url='https://github.com/anooplab/distances',
+    license='GPL v3',
+    author='A. Anoop',
+    author_email='anoop@chem.iitkgp.ac.in',
+    description=''
+)
---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    Distance Analysis
+    ~~~~~~~~~~~~~~~~~
+    
+    This function reads an XYZ file and returns the coordinates, atoms list and molecule name.
+    
+    Parameters:
+        filename (str): The file name of the XYZ file
+    
+    Returns:
+        atoms_list (list): A list of atoms in the molecule
+        mol_coordinates (numpy.ndarray): The coordinates of the molecule
+        mol_name (str): The name of the molecule
+"""
